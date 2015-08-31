a453f5e33f40fa86967093b69183b20899b59829
chenwei <806000460@qq.com>
2015-08-31 16:02:27 +0800

    add help

--- pattern1/TakeInput.java
+++ pattern1/TakeInput.java
@@ -1,6 +1,8 @@
 package pattern1;
 
+import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -50,6 +52,20 @@ public class TakeInput {
 		else if(parser.equalsIgnoreCase("Exit")){
 			System.out.println("See you again. Have a nice day!");
 			System.exit(0);
+		}
+		else if(parser.equalsIgnoreCase("help")) {
+			File file = new File("README.txt");
+			try {
+				if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+					String cmd = "rundll32 url.dll,FileProtocolHandler " + file.getCanonicalPath();
+					Runtime.getRuntime().exec(cmd);
+				} else {
+					Desktop.getDesktop().edit(file);
+				}
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
 		}
 		else{
 			System.out.println("Wrong Command!!");
